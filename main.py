5abc64015ab7025af32d655e98d60dc88806e715
Joseph Dandrea <joseph.dandrea@gmail.com>
2025-11-23 19:45:45 -0500

    Final correct version

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from stats import get_num_words
-from stats import get_num_chars
-from stats import sorted_list_dict
+from stats import get_num_words, get_num_chars, chars_dict_to_sorted_list
 
 def get_book_text():
     with open("books/frankenstein.txt") as b:
@@ -11,11 +9,18 @@ def main():
     print("============ BOOKBOT ============")
     print("Analyzing book found at books/frankenstein.txt...")
     print("----------- Word Count ----------")
-    words = get_num_words(get_book_text())
-    print(f'Found {words} total words.')
+    text = get_book_text()
+    words = get_num_words(text)
+    char_dict = get_num_chars(text)
+    print(f'Found {words} total words')
     print("--------- Character Count -------")
-    char_count = sorted_list_dict(shar_dict) )
-    print(f'Found {char_count} total characters.')
+    chars_sorted_list = chars_dict_to_sorted_list(char_dict)
+    for item in chars_sorted_list:
+        ch = item['char']
+        if not ch.isalpha():
+            continue
+        print(f"{ch}: {item['num']}")
+    print("============= END ===============")
 
          
 
--- stats.py
+++ stats.py
@@ -4,10 +4,9 @@ def get_num_words(text):
 
 #get the number of occurences of characters in a text inlcuding symbols and spaces
 
-char_dict = {}
-char_count = 0
 
 def get_num_chars(text):
+    char_dict = {}
     text = text.lower()
     for char in text:
         if char in char_dict:
@@ -16,13 +15,22 @@ def get_num_chars(text):
             char_dict[char] = 1
     return char_dict
 
-dict_list = []
+def sort_on(item):
+    return item['num']
 
-def sorted_list_dict(char_dict): 
+def chars_dict_to_sorted_list(char_dict): 
+    
+    dict_list = []
+    
     for char, count in char_dict.items():
         small_dict = {"char": char, "num": count}
         dict_list.append(small_dict)
-    return dict_list
-                 
+    
+    dict_list.sort(reverse=True, key=sort_on)
+    
+    return dict_list    
+
+
+
 
 
